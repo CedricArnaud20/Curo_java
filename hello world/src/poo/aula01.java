496f7c4535544822b2acae0c01f6a17da0d730b1
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2021-08-26 22:44:34 -0300

    hello world

---
+++ hello world/src/poo/aula01.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class aula01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello world");
+        
+	}
+
+}
